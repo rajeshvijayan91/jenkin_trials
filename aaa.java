6fdd951b27a3c15571f3a16e86079830cdacd7ed
Rajesh Vijayan <rajesh.vijayan@stellapps.com>
2019-05-20 13:29:36 +0530

    First Commit

---
+++ aaa.java
@@ -0,0 +1,19 @@
+class AAA{
+public static void main (String args[])
+{
+for( int i=1;i<10;i++)
+{	
+	for (int j=0;j<i;j++)
+	{System.out.print("*\t");}
+	System.out.println(" \n");
+
+}
+for( int i=10;i>1;i--)
+{
+        for (int j=0;j<i;j++)
+        {System.out.print("*\t");}
+        System.out.println(" \n");
+
+}
+}
+}
